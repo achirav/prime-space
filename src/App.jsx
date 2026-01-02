ea25f740230e46dff422ce9493e177d9134207d9
achirav <achirav22@gmail.com>
2026-01-02 21:43:56 +0530

    updated with components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import HomePage from "./assets/HomePage";
 import Layout from "./assets/Layout";
 import SearchPage from "./assets/SearchPage";
 import PropertyDetails from "./assets/PropertyDetails";
+import ContactAgent from "./assets/ContactAgent";
+import ContactForm from "./assets/ContactForm";
+
+
 
 function App() {
   return (
@@ -11,6 +15,8 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="search" element={<SearchPage />} />
         <Route path="property/:id" element={<PropertyDetails />} />
+        <Route path="contact" element={<ContactAgent />} />
+        <Route path="contact-form" element={<ContactForm />} />
       </Route>
     </Routes>
   );
