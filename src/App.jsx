e49db051a56bf968d6dee79c1c9c4300a2ba61c0
achirav <achirav22@gmail.com>
2026-01-02 15:17:48 +0530

    Add Search Page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./assets/HomePage";
 import Layout from "./assets/Layout";
+import SearchPage from "./assets/SearchPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="search" element={<SearchPage />} />
       </Route>
     </Routes>
   );
