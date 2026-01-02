572359c8c831b09f028897496bfb1bead237581f
achirav <achirav22@gmail.com>
2026-01-02 14:23:32 +0530

    Property 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./assets/HomePage";
+import Layout from "./assets/Layout";
 
 function App() {
   return (
     <Routes>
-      
+      <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-      
+      </Route>
     </Routes>
   );
 }
