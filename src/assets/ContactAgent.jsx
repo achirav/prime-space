bd9508879b4ac3511a7c0ad098f5d82dc144b683
achirav <achirav22@gmail.com>
2026-01-02 21:51:01 +0530

    Contact Page updated

--- src/assets/ContactAgent.jsx
+++ src/assets/ContactAgent.jsx
@@ -9,8 +9,8 @@ function ContactAgent() {
       <section className="contact-hero">
         <h1>Get in Touch</h1>
         <p>
-          Speak with our experienced real estate team and discover premium
-          living opportunities with Opulenza.
+          Engage with our trusted real estate team and unlock premium 
+          property opportunities with Prime Space.
         </p>
       </section>
 
@@ -18,17 +18,17 @@ function ContactAgent() {
       <section className="contact-info-grid">
         <div className="info-card">
           <h3>Call Us</h3>
-          <p>+94 76 123 4567</p>
+          <p>+94 76 887 1461</p>
         </div>
 
         <div className="info-card">
           <h3>Email</h3>
-          <p>info@opulenza.lk</p>
+          <p>info@primespace.lk</p>
         </div>
 
         <div className="info-card">
           <h3>Visit Office</h3>
-          <p>123 Lotus Grove Lane,<br />Colombo 07, Sri Lanka</p>
+          <p>142 Primrose Road,<br />Kandy, Sri Lanka</p>
         </div>
       </section>
 
@@ -38,8 +38,8 @@ function ContactAgent() {
         {/* Map */}
         <div className="map-box">
           <iframe
-            title="Opulenza Location"
-            src="https://www.google.com/maps?q=Colombo+7,+Colombo,+Sri+Lanka&z=15&output=embed"
+            title="PrimeSpace Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7915.287413122161!2d80.60860993924551!3d7.281318463260515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3689391b468ed%3A0xfabafcaf980cb7f5!2sPrimrose%2C%20Kandy!5e0!3m2!1sen!2slk!4v1767370704741!5m2!1sen!2slk" 
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
